bd040a3329cd0d7fe504b99604234f99627a355a
Marco <10698406@polimi.it>
2022-04-03 17:43:08 +0200

    Added JavaDoc for MNStrategy package

--- src/main/java/it/polimi/ingsw/model/mnstrategy/MNStrategy.java
+++ src/main/java/it/polimi/ingsw/model/mnstrategy/MNStrategy.java
@@ -2,6 +2,14 @@ package it.polimi.ingsw.model.mnstrategy;
 
 import it.polimi.ingsw.model.player.Assistant;
 
+/**
+ * Defines the method used to obtain the number of steps from an Assistant
+ */
 public interface MNStrategy {
-    public int mnSteps(Assistant a);
+    /**
+     * Returns the number of steps the current Player can make
+     * @param a Assistant played
+     * @return number of steps MN can make
+     */
+    int mnSteps(Assistant a);
 }
