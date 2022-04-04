0dbc0ff47e53a86f46fd5244c4b3818f3a64a084
Marco <10698406@polimi.it>
2022-04-04 14:50:33 +0200

    Updated CharacterWithStudent to accept ColorS

--- src/test/java/it/polimi/ingsw/model/character/CharacterWithStudentTest.java
+++ src/test/java/it/polimi/ingsw/model/character/CharacterWithStudentTest.java
@@ -14,13 +14,13 @@ class CharacterWithStudentTest {
     @Test
     void add() {
         CharacterWithStudent c = new CharacterWithStudent(1, "test",  4);
-        c.add(new Student(ColorS.BLUE));
+        c.add(ColorS.BLUE);
     }
 
     @Test
     void remove() {
         CharacterWithStudent c = new CharacterWithStudent(1, "test",  4);
-        c.add(new Student(ColorS.BLUE));
-        c.remove(new Student(ColorS.BLUE));
+        c.add(ColorS.BLUE);
+        c.remove(ColorS.BLUE);
     }
 }
