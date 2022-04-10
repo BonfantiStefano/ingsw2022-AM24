337d546298cd6a8238a30845fdd63fe37ec5928f
AnnaChyz <1068227@polimi.it>
2022-04-10 10:30:10 +0200

    javadoc InvalidIndexException

--- src/main/java/it/polimi/ingsw/exceptions/InvalidIndexException.java
+++ src/main/java/it/polimi/ingsw/exceptions/InvalidIndexException.java
@@ -1,7 +1,9 @@
 package it.polimi.ingsw.exceptions;
 
 public class InvalidIndexException extends Throwable {
-
+    /**
+     * Class InvalidIndexException is thrown when the index position on the card doesn't exist in the player's hand
+     */
     @Override
     public String getMessage() {
         return ("Error: Choose a card between 1 and 10");
